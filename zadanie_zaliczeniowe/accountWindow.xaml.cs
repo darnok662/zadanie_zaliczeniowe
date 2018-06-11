4199207b13a32774ea64815ef267f2ddc4deb3c4
Konrad Paszkiewicz <darnok662@hotmail.com>
2018-06-12 00:11:11 +0200

    Full UI designed

--- zadanie_zaliczeniowe/accountWindow.xaml.cs
+++ zadanie_zaliczeniowe/accountWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace zadanie_zaliczeniowe
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for accountWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class accountWindow : Window
     {
-        public MainWindow()
+        public accountWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
--- zadanie_zaliczeniowe/clientWindow.xaml.cs
+++ zadanie_zaliczeniowe/clientWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace zadanie_zaliczeniowe
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for clientWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class clientWindow : Window
     {
-        public MainWindow()
+        public clientWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
